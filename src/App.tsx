600df668fc0588d251d495c3d34882082aed591f
Cobicobe <jdanforthco3@gmail.com>
2025-06-17 02:04:50 -0500

    Create App.tsx
    Main UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import OrderForm from './components/OrderForm'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Comanche Hometown Delivery</h1>
+      <OrderForm />
+    </div>
+  )
+}
+
+export default App
